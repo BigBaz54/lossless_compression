a7feb7cb017602bd15f69d2f6f0b407afb68a5d8
BigBaz <louisvincent@famillecapelli.fr>
2023-01-25 11:40:26 +0100

    [+] generate binary and alphanumeric messages

---
+++ generate_message.py
@@ -0,0 +1,15 @@
+from random import *
+
+def generate_binary(length):
+    return ''.join([str(randint(0, 1)) for _ in range(length)])
+
+def generate_alphanumeric(length):
+    lower = [chr(i+ord('a')) for i in range(26)]
+    upper = [chr(i+ord('A')) for i in range(26)]
+    digits = [str(i) for i in range(10)]
+    alphanumeric = lower + upper + digits
+    return ''.join([alphanumeric[randint(0, len(alphanumeric)-1)] for _ in range(length)])
+
+if __name__ == '__main__':
+    print(generate_binary(100));
+    print(generate_alphanumeric(100));
