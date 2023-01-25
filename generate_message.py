cd8c9ee9874d5ffc2b671555d9671f3e5cc0e916
BigBaz <louisvincent@famillecapelli.fr>
2023-01-25 14:51:57 +0100

    [+] write in file

--- generate_message.py
+++ generate_message.py
@@ -7,7 +7,7 @@ def generate_alphanumeric(length):
     lower = [chr(i+ord('a')) for i in range(26)]
     upper = [chr(i+ord('A')) for i in range(26)]
     digits = [str(i) for i in range(10)]
-    alphanumeric = lower + upper + digits
+    alphanumeric = lower + upper + digits + [' ']
     return ''.join([alphanumeric[randint(0, len(alphanumeric)-1)] for _ in range(length)])
 
 if __name__ == '__main__':
